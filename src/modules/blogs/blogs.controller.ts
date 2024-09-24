4704a3cdcc513aef485b020528dd7c5a4a3420e8
lvy <upwardsrwr@gmail.com>
2024-09-24 16:37:00 +0800

    feat: add blogs

--- src/modules/blogs/blogs.controller.ts
+++ src/modules/blogs/blogs.controller.ts
@@ -1,32 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { BlogsService } from './blogs.service';
 import { CreateBlogDto } from './dto/create-blog.dto';
 import { UpdateBlogDto } from './dto/update-blog.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import type { Prisma } from '@prisma/client';
+import { query } from 'express';
 
+@ApiTags('blogs')
 @Controller('blogs')
 export class BlogsController {
   constructor(private readonly blogsService: BlogsService) {}
 
+  @ApiOperation({ summary: '创建blogs' })
   @Post()
-  create(@Body() createBlogDto: CreateBlogDto) {
+  create(@Body() createBlogDto: Prisma.BlogsCreateInput) {
     return this.blogsService.create(createBlogDto);
   }
 
+  @ApiOperation({ summary: '得到所有类型blogs' })
   @Get()
-  findAll() {
-    return this.blogsService.findAll();
+  findAll(@Query() siteType: string) {
+    return this.blogsService.findAll(siteType);
   }
 
+  @ApiOperation({ summary: '得到单个blogs' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.blogsService.findOne(+id);
   }
 
+  @ApiOperation({ summary: '更新blogs' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateBlogDto: UpdateBlogDto) {
     return this.blogsService.update(+id, updateBlogDto);
   }
 
+  @ApiOperation({ summary: '删除blogs' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.blogsService.remove(+id);
--- src/modules/blogs/blogs.service.ts
+++ src/modules/blogs/blogs.service.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBlogDto } from './dto/create-blog.dto';
 import { UpdateBlogDto } from './dto/update-blog.dto';
+import { PrismaService } from 'src/shared/prisma/prisma.service';
+import { Blogs, Prisma } from '@prisma/client';
 
 @Injectable()
 export class BlogsService {
-  create(createBlogDto: CreateBlogDto) {
-    return 'This action adds a new blog';
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createBlogDto: Prisma.BlogsCreateInput) {
+    return await this.prismaService.blogs.create({ data: createBlogDto });
   }
 
-  findAll() {
-    return `This action returns all blogs`;
+  async findAll(blogsFindManyArgs: Prisma.BlogsFindManyArgs) {
+    return await this.prismaService.blogs.findMany({
+      where: {
+        ...blogsFindManyArgs,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} blog`;
+  findOne(blogsWhereUniqueInput: Prisma.BlogsWhereUniqueInput) {
+    return this.prismaService.blogs.findUnique({
+      where: blogsWhereUniqueInput,
+    });
   }
 
-  update(id: number, updateBlogDto: UpdateBlogDto) {
-    return `This action updates a #${id} blog`;
+  async update(params: {
+    where: Prisma.BlogsWhereUniqueInput;
+    data: Prisma.BlogsUpdateInput;
+  }): Promise<Blogs> {
+    const { data, where } = params;
+    return this.prismaService.blogs.update({
+      data,
+      where,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} blog`;
+  async remove(where: Prisma.BlogsWhereUniqueInput): Promise<Blogs> {
+    return this.prismaService.blogs.delete({
+      where,
+    });
   }
 }
